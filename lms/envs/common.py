6e91c538fc087eba3a5612c7352112c0acbcabc2
Thomas Toye <thomastoye@users.noreply.github.com>
2015-06-12 13:28:33 +0200

    Turn on search by default

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -355,10 +355,10 @@ FEATURES = {
     'MODE_CREATION_FOR_TESTING': False,
 
     # Courseware search feature
-    'ENABLE_COURSEWARE_SEARCH': False,
+    'ENABLE_COURSEWARE_SEARCH': True,
 
     # Dashboard search feature
-    'ENABLE_DASHBOARD_SEARCH': False,
+    'ENABLE_DASHBOARD_SEARCH': True,
 
     # log all information from cybersource callbacks
     'LOG_POSTPAY_CALLBACKS': True,
@@ -385,7 +385,7 @@ FEATURES = {
     },
 
     # Course discovery feature
-    'ENABLE_COURSE_DISCOVERY': False,
+    'ENABLE_COURSE_DISCOVERY': True,
 
     # Software secure fake page feature flag
     'ENABLE_SOFTWARE_SECURE_FAKE': False,
@@ -2370,7 +2370,7 @@ ACCOUNT_VISIBILITY_CONFIGURATION = {
 # E-Commerce API Configuration
 ECOMMERCE_PUBLIC_URL_ROOT = None
 ECOMMERCE_API_URL = None
-ECOMMERCE_API_SIGNING_KEY = None
+ECOMMERCE_API_SIGNING_KEY = 'search.tests.mock_search_engine.MockSearchEngine'
 ECOMMERCE_API_TIMEOUT = 5
 
 # Reverification checkpoint name pattern
